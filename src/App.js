615f3d36e92fe858e92df9c55a3a1771b631073d
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-10-09 19:08:14 +0600

    new commit:

--- src/App.js
+++ src/App.js
@@ -11,14 +11,25 @@ class App extends Component {
     'capptain america'
   ]
 
+  componentDidMount(){
+    fetch("http://localhost:8000/movie/",{
+      method: 'GET',
+      headers: {
+        Authorization: "Token 314554a0acf7f1b21822478c3b2c291057157583",
+      }
+    }).then( resp => resp.json())
+    .then( res => console.log(res))
+    .catch(error => console.log(error))
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <p>
             Movie Rater
-          <MovieList movie={this.new_movies} />
           </p>
+          <MovieList movie={this.new_movies} />
         </header>
       </div>
     );
